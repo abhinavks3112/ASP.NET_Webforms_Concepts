3e5554e3d4eb989af8ad3453cac46dafea9379b1
Abhinav <abhiks3112@gmail.com>
2020-04-15 21:23:32 +0530

    ApplicationSessionWebForm: Application and session state concept

---
+++ WebFormBasics/WebFormBasics/ApplicationSessionWebForm.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormBasics
+{
+    public partial class ApplicationSessionWebForm : System.Web.UI.Page
+    {
+        private const string TOTAL_APPLICATIONS_KEY = "TotalApplications";
+        private const string TOTAL_USER_SESSIONS_KEY = "TotalUserSessions";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Open 2 or more browser one after another and see the results
+            Response.Write("Number of Applications: " + Application[TOTAL_APPLICATIONS_KEY]);
+            Response.Write("<br />");
+            Response.Write("Number of users Online: " + Application[TOTAL_USER_SESSIONS_KEY]);
+        }
+    }
+}
--- WebFormBasics/WebFormBasics/Global.asax.cs
+++ WebFormBasics/WebFormBasics/Global.asax.cs
@@ -11,11 +11,32 @@ namespace WebFormBasics
 {
     public class Global : HttpApplication
     {
+        private const string TOTAL_APPLICATIONS_KEY = "TotalApplications";
+        private const string TOTAL_USER_SESSIONS_KEY = "TotalUserSessions";
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Create and initialize Application state variables
+            Application[TOTAL_APPLICATIONS_KEY] = 0;
+            Application[TOTAL_USER_SESSIONS_KEY] = 0;
+
+            // Increment Total Applications by 1
+            Application[TOTAL_APPLICATIONS_KEY] = Convert.ToInt32(Application[TOTAL_APPLICATIONS_KEY]) + 1;
+        }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            // Increment Total User Sessions by 1
+            Application[TOTAL_USER_SESSIONS_KEY] = Convert.ToInt32(Application[TOTAL_USER_SESSIONS_KEY]) + 1;
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+            // Decrement Total User Sessions by 1
+            Application[TOTAL_USER_SESSIONS_KEY] = Convert.ToInt32(Application[TOTAL_USER_SESSIONS_KEY]) - 1;
         }
     }
 }
