eed34c9f8ab51776159df5c7d6f2bf46ebc17563
Abhinav <abhiks3112@gmail.com>
2020-04-16 22:12:48 +0530

    ButtonEventsWebForm: Button Click and Command events concept shown

---
+++ WebFormBasics/WebFormBasics/ButtonEventsWebForm.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ButtonEventsWebForm.aspx.cs" Inherits="WebFormBasics.ButtonEventsWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnPrint" runat="server" Text="Print" oncommand="ButtonCommand_Click" CommandName="Print" />
+
+            <%-- Client side click event(OnClientClick) and its handler written here , using javascript or any language that a browser understands --%>
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" oncommand="ButtonCommand_Click" CommandName="Delete" 
+                OnClick="Button_Click"  OnClientClick="return confirm('Are you sure you want to delete this item?')"/>
+
+            <%-- Event handler on server side is statically hooked up with event here(eg. oncommand="ButtonCommand_Click") eg.  --%>
+            <asp:Button ID="btnTop10Emp" runat="server" Text="Show Top 10 Employees" oncommand="ButtonCommand_Click"
+               CommandName="Show" CommandArgument="Top10"  />
+
+            <asp:Button ID="btnBottom10Emp" runat="server" Text="Show Bottom 10 Employees" oncommand="ButtonCommand_Click" 
+               CommandName="Show" CommandArgument="Bottom10"/>
+
+            <asp:Label ID="lblOutput" runat="server" Text="Output"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFormBasics/WebFormBasics/ButtonEventsWebForm.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormBasics
+{
+    public partial class ButtonEventsWebForm : System.Web.UI.Page
+    {
+        private const string _click_Button_Msg = "Button Clicked!! <br /><br /><hr>";
+        private const string _cmd_Print = "Print";
+        private const string _cmd_Print_Button_Click_Msg = "Print button clicked.<br /><br /><hr>";
+        private const string _cmd_Delete = "Delete";
+        private const string _cmd_Delete_Button_Click_Msg = "Delete button clicked.<br /><br /><hr>";
+        private const string _cmd_Show = "Show";
+        private const string _cmd_Args_Show_Top10 = "Top10";
+        private const string _cmd_Show_Top10_Button_Click_Msg = "Show Top 10 Employees button clicked.<br /><br /><hr>";
+        private const string _cmd_Args_Show_Bottom10 = "Bottom10";
+        private const string _cmd_Show_Bottom10_Button_Click_Msg = "Show Bottom 10 Employees button clicked.<br /><br /><hr>";
+        private const string _cmd_Default_Click_Msg = "Unknown button clicked!<br /><br /><hr>";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /* We can dynamically assign event handlers to control events using different delegates 
+             for different events eg. EventHandler delegate for click event, CommandEventHandler for control's
+             command event.
+             Alternatively, we can assign them statically on Design Page using property or on source page.
+             Eg. OnClick="Button_Click"
+            */
+            // btnDelete.Click +=  new EventHandler(Button_Click);
+
+            // btnPrint.Command += new CommandEventHandler(ButtonCommand_Click);
+        }
+
+        /* 
+        * When button is clicked both click and command events get raised.
+        * Click event gets called before command event. 
+        */
+        /*
+         * Command event, makes it possible to have a single event handler method responding to the click event
+         * of multiple buttons. The command event, CommandName and CommandArgument properties are extremely useful
+         * when working with data-bound controls like Repeater, GridView, DataList, etc.
+        */
+        protected void ButtonCommand_Click(object sender, CommandEventArgs e)
+        {
+            switch (e.CommandName)
+            {
+                case _cmd_Print:
+                    lblOutput.Text = _cmd_Print_Button_Click_Msg;
+                    break;
+                case _cmd_Delete:
+                    lblOutput.Text = _cmd_Delete_Button_Click_Msg;
+                    break;
+                case _cmd_Show:
+                    if (e.CommandArgument.ToString() == _cmd_Args_Show_Top10)
+                    {
+                        lblOutput.Text = _cmd_Show_Top10_Button_Click_Msg;
+                    }
+                    else if (e.CommandArgument.ToString() == _cmd_Args_Show_Bottom10)
+                    {
+                        lblOutput.Text = _cmd_Show_Bottom10_Button_Click_Msg;
+                    }
+                    break;
+                default:
+                    lblOutput.Text = _cmd_Default_Click_Msg;
+                    break;
+            }
+        }
+
+        /* Server side click event written here */
+        /* 
+         * When button is clicked both click and command events get raised.
+         * Click event gets called before command event. 
+        */
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            Response.Write(_click_Button_Msg);
+        }
+    }
+}
